c7699fde434c6d5f0532d9ec50dd46ad004377be
danilapog <danil.titarenko@onlyoffice.com>
2025-08-12 09:20:01 +0000

    Fix vagrant box versioning (#11)
    Co-authored-by: danilapog <danil.titarenko@onlyoffice.com>
    Co-committed-by: danilapog <danil.titarenko@onlyoffice.com>

--- .github/scripts/vagrant_publish.sh
+++ .github/scripts/vagrant_publish.sh
@@ -5,7 +5,7 @@ date=$(date +%F)
 
 vagrant cloud publish \
 	           ${VAGRANT_ORG}/${PRODUCT}-${OS_NAME} \
-                   $date virtualbox ${PRODUCT}-${OS_NAME}.box \
+                   ${BOX_VERSION} virtualbox ${PRODUCT}-${OS_NAME}.box \
                    -d "Box with pre-installed ${PRODUCT}" \
                    --version-description "${PRODUCT}:${BOX_VERSION}" \
                    --release --short-description "Boxes for update testing" \
